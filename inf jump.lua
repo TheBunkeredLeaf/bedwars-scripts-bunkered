09bedf8694967e1a73b2e4e2c957a943c50808bc
BunkeredLeaf <104906800+TheBunkeredLeaf@users.noreply.github.com>
2023-10-14 09:12:23 -0400

    Update inf jump.lua

--- inf jump.lua
+++ inf jump.lua
@@ -1,10 +1,9 @@
 Events.MatchStart(function(event)
     local startMessage = "inf jump yessir"
     local color = Color3.fromRGB(0, 0, 0)
-    AnnouncementService.sendAnnouncement(startMessage, color)
-    
-end)
---set all players to have inf jumps
+    AnnouncementService.sendAnnouncement(startMessage, color)--set all players to have inf jumps
 for i, player in ipairs(PlayerService.getPlayers()) do 
     player:registerAdditionalAirJumps("bounce", 99999)
 end
+    
+end)
